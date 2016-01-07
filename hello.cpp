ce836f8633ffaf249dc726313c4edd879b8107be
SiminWang89 <wangsimin89@gmail.com>
2016-01-07 16:50:38 -0500

    include files

---
+++ hello.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <iostream>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_combination.h>
+#include <gsl/gsl_complex.h>
+#include <gsl/gsl_complex_math.h>
+#include <gsl/gsl_block.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_multimin.h>
+#include <gsl/gsl_sf_coupling.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_sort.h>
+#include <gsl/gsl_sort_vector.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_chebyshev.h>
+#include <gsl/gsl_sf_legendre.h>
+#include <gsl/gsl_sf_laguerre.h>
+#include <gsl/gsl_sf_hyperg.h>
+#include <gsl/gsl_sf_pow_int.h>
+#include <gsl/gsl_sf_erf.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_fft_real.h>
+#include <gsl/gsl_fft_halfcomplex.h>
+#include <gsl/gsl_bspline.h>
+#include <gsl/gsl_odeiv2.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_fft_complex.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_roots.h>
+#include <armadillo>
+using namespace std;
+
+int main ()
+{
+    cout<<"hello world!"<<endl;
+
+    return 1;
+}
